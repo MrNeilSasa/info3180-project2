41ffa6fb32cebe544f71a3aa75cdea56c62b3caf
RyanAsh <ryanash.b@gmail.com>
2023-04-25 20:39:30 -0500

    Forms Created

--- app/views.py
+++ app/views.py
@@ -6,8 +6,14 @@ This file creates your application.
 """
 
 from app import app
-from flask import render_template, request, jsonify, send_file
+from flask import render_template, request, jsonify, send_file, flash, redirect, url_for
 import os
+from flask_login import login_user, logout_user, current_user, login_required
+from app import app, db
+from app.forms import LoginForm
+from app.models import User
+from werkzeug.security import check_password_hash
+
 
 
 ###
@@ -17,6 +23,46 @@ import os
 @app.route('/')
 def index():
     return jsonify(message="This is the beginning of our API")
+
+# @app.route('/login', methods=['POST', 'GET'])
+# def login():
+#     form = LoginForm()
+
+#     # change this to actually validate the entire form submission
+#     # and not just one field
+#     if form.validate_on_submit():
+#         username = form.username.data
+#         password = form.password.data
+#         # Get the username and password values from the form.
+
+#         # Using your model, query database for a user based on the username
+#         user = db.session.execute(db.select(User).filter_by(username=username)).scalar()
+#         # and password submitted. Remember you need to compare the password hash.
+#         # You will need to import the appropriate function to do so.
+#         # Then store the result of that query to a `user` variable so it can be
+#         # passed to the login_user() method below.
+
+#         # Gets user id, load into session
+#         if user is not None and check_password_hash(user.password, password):
+#             remember_me = False
+
+#             if 'remember_me' in request.form:
+#                 remember_me = True
+
+#             # If the user is not blank, meaning if a user was actually found,
+#             # then login the user and create the user session.
+#             # user should be an instance of your `User` class
+#             login_user(user, remember=remember_me)
+
+#             flash('Logged in successfully.', 'success')
+
+#             next_page = request.args.get('next')
+#             return redirect(next_page or url_for('home'))
+#         else:
+#             flash('Username or Password is incorrect.', 'danger')
+
+#     flash_errors(form)
+#     return render_template("login.html", form=form)
 
 
 ###
@@ -56,6 +102,13 @@ def add_header(response):
     response.headers['Cache-Control'] = 'public, max-age=0'
     return response
 
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+), 'danger')
 
 @app.errorhandler(404)
 def page_not_found(error):
